dfdca79c3a50323e1e25693677bb7758bd747b55
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2017-04-11 11:10:43 +0200

    Adding smoke tests for the sub commands

--- .travis/test.sh
+++ .travis/test.sh
@@ -3,5 +3,8 @@ set -exuo pipefail
 
 export LD_LIBRARY_PATH=$(readlink -f inst-deps)/lib
 
-./inst/bin/cnvetti --help
 ./inst/bin/cnvetti --version
+./inst/bin/cnvetti --help
+./inst/bin/cnvetti coverage --help
+./inst/bin/cnvetti normalize --help
+./inst/bin/cnvetti background --help
